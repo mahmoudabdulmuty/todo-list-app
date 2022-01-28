b8b974a27c6a34781842b0e9e1ec7facc5c2567f
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-28 22:02:57 +0200

    add trash icon

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { nanoid } from 'nanoid';
 import { Component } from 'react';
+import { TiTrash } from 'react-icons/ti';
 import './App.css';
 
 class App extends Component {
@@ -18,13 +19,15 @@ class App extends Component {
 			}
 		});
 	};
-	handleSubmit = () => {
-		let item = this.state.item;
-		let items = this.state.items;
-		items.push(item);
-		this.setState({
-			items
-		});
+	handleAdd = () => {
+		if (this.state.item.todo.length > 0) {
+			let item = this.state.item;
+			let items = this.state.items;
+			items.push(item);
+			this.setState({
+				items
+			});
+		}
 	};
 	handleDelete = (id) => {
 		let items = this.state.items.filter((item) => {
@@ -36,21 +39,33 @@ class App extends Component {
 	};
 	render() {
 		return (
-			<div className="App">
-				<input
-					onChange={this.handleChange}
-					type="text"
-					name="todo"
-					value={this.state.item.todo}
-				/>
-				<button onClick={this.handleSubmit}>Submit</button>
-				{this.state.items.map(({ todo, id }) => (
-					<div key={id}>
-						<span>{todo}</span>
-						<button onClick={() => this.handleDelete(id)}>delete</button>
-					</div>
-				))}
-			</div>
+			<>
+				<header>
+					<h1>Todo List</h1>
+					<input
+						onChange={this.handleChange}
+						type="text"
+						name="todo"
+						value={this.state.item.todo}
+						placeholder="Things to be done..."
+					/>
+					<span className="addBtn" onClick={this.handleAdd}>
+						Add
+					</span>
+				</header>
+				<ul>
+					{this.state.items.length > 0 &&
+						this.state.items.map(({ todo, id }) => (
+							<li key={id}>
+								<span>{todo}</span>
+								<TiTrash
+									className="trashIcon"
+									onClick={() => this.handleDelete(id)}
+								></TiTrash>
+							</li>
+						))}
+				</ul>
+			</>
 		);
 	}
 }
