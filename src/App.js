375dc3d446ede69590449c8d5a8c0548275f8974
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-28 21:09:36 +0200

    initial commit : added all files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		item: {
+			todo: ''
+		},
+		items: []
+	};
+	handleChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({
+			item: {
+				[name]: value,
+				id: nanoid()
+			}
+		});
+	};
+	handleSubmit = () => {
+		let item = this.state.item;
+		let items = this.state.items;
+		items.push(item);
+		this.setState({
+			items
+		});
+	};
+	handleDelete = (id) => {
+		let items = this.state.items.filter((item) => {
+			return item.id !== id;
+		});
+		this.setState({
+			items: items
+		});
+	};
+	render() {
+		return (
+			<div className="App">
+				<input
+					onChange={this.handleChange}
+					type="text"
+					name="todo"
+					value={this.state.item.todo}
+				/>
+				<button onClick={this.handleSubmit}>Submit</button>
+				{this.state.items.map(({ todo, id }) => (
+					<div key={id}>
+						<span>{todo}</span>
+						<button onClick={() => this.handleDelete(id)}>delete</button>
+					</div>
+				))}
+			</div>
+		);
+	}
 }
 
 export default App;
